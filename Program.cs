48826d5c296a56a8f2b5e093d121de10c46368eb
skienterprises-da <douglas_andrzejewski@ski-network.net>
2022-01-20 13:27:23 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IT481_Unit_6_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
